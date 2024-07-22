e3c2bb6c76160dbcf2bff397fec70557e725882e
Serhii K <serhii.kozachenko.92@gmail.com>
2024-07-22 23:33:44 +0300

    Added windows test script for ex03

---
+++ test_ex03.bat
@@ -0,0 +1,5 @@
+python.exe .\ex03.py .\file_that_does_not_exist.log
+python.exe .\ex03.py .\log_for_ex03.log
+python.exe .\ex03.py .\log_for_ex03.log WARNING
+python.exe .\ex03.py .\log_for_ex03.log VERBOSE
+@pause
